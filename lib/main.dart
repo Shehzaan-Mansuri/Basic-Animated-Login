b3e6a4b2e316759e8b2f23e244fba98393e579f7
Shehzaan-Mansuri <shehzaanmansuri1@gmail.com>
2021-07-15 21:36:35 +0530

    Flutter Basic Animated Login UI

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: LoginPage(),
       theme: ThemeData(primarySwatch: Colors.blue),
     );
